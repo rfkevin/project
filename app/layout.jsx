b137aae964f6d063741dd0625388574fffd1edfd
rakotoarison kevin <rfahedkevin@gmail.com>
2023-05-30 14:05:16 +0200

    de

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,20 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import './global.css'
+import { Inter } from 'next/font/google';
 export const metadata = {
   title: 'Detachez-vous',
   description: 'to do',
 }
 import Footer from "@/components/footer";
-import Provider from "@/components/providers";
+import Provider from "@/app/components/providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Provider>
             {children}
         </Provider>
