df9eaaa82696f15cf625e72dc50aeac2c0ef9187
mouloud <mouloud.ait.1996@gmail.com>
2023-06-15 10:39:11 +0200

    darino form

--- app/components/Form/UserForm.jsx
+++ app/components/Form/UserForm.jsx
@@ -1,19 +1,167 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import styles from './productForm.module.css';
+import { addProduct } from "../../../endPoints/apiEndpoints";
 
-export const UserForm = ({ setShowForm }) => {
+
+export default function Home() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    userName: '',
+    address: '',
+    phone: '',
+    birth: '',
+    password: '',
+    email: '',
+  });
+      
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value
+        }));
+      };
+      
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formData);
+        const result = await addUser(formData);
+        console.log(result);
+      };
+      
   return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+    <form>
     <div>
-      <form>
-        {/* Ajoutez vos champs de formulaire ici */}
-        <input type="text" placeholder="Full name" />
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Address" />
-        <input type="number" placeholder="Phone" />
-        <input type="number" placeholder="Date of birth"/>
-        <input type="email" placeholder="Email" />
-        <button type="submit">Modify a user</button>
-      </form>
-      <button onClick={() => setShowForm(false)}>Close</button>
-    </div>
+    <TextField
+
+  label="Title"
+  id="filled-size-small"
+  defaultValue=""
+  variant="filled"
+  size="small"
+  onChange={handleChange}
+  value={formData.title}
+  name="title"
+/>
+<TextField
+
+  label="Subtitle"
+  id="filled-size-small"
+  defaultValue=""
+  variant="filled"
+  size="small"
+  onChange={handleChange}
+  value={formData.subtitle}
+  name="subtitle"
+/>
+<TextField
+
+  label="Quantity"
+  id="filled-size-small"
+  defaultValue=""
+  variant="filled"
+  size="small"
+  onChange={handleChange}
+  value={formData.quantity}
+  name="quantity"
+/>
+      </div>
+      <div>
+        <TextField
+        
+          label="Categories"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.categories}
+          name="categories"
+        />
+        <TextField
+        
+          label="Type"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.type}
+          name="type"
+        />
+      </div>
+      <div>
+        <TextField
+        
+          label="Marque"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.marque}
+          name="marque"
+        />
+        <TextField
+        
+          label="Years"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.years}
+          name="years"
+        />
+      </div>
+      <div>
+        <TextField
+        
+          label="Raters"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.raters}
+          name="raters"
+        />
+        <TextField
+        
+          label="Price"
+          id="filled-size-small"
+          defaultValue=""
+          variant="filled"
+          size="small"
+          onChange={handleChange}
+          value={formData.price}
+          name="price"
+        />
+        
+      </div>
+      
+    <ul className={`${styles.ul}`}>
+      
+      <Button onClick={handleSubmit} variant="contained" color="success">
+        Ajouter
+    </Button>
+    <Button onClick={() => setShowForm(false)} variant="contained" color="error">
+        Annuler
+    </Button>
+    </ul>
+    </form>
+    </Box>
   );
 };
